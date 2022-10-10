821989d661a4ab63dc22432985a615ee9c2b40cc
unknown <oyinjoe23@gmail.com>
2022-10-10 23:55:24 +0100

    setting current time

--- Assets/js/Timer.js
+++ Assets/js/Timer.js
@@ -1,154 +1,194 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-export default class Timer {
-  constructor(root) {
-    root.innerHTML = Timer.getHtml();
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+// const weekdays = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// export default class Timer {
+//   constructor(root) {
+//     root.innerHTML = Timer.getHtml();
 
-    this.el = {
-      months: root.querySelector(".months"),
-      weeks: root.querySelector(".weeks"),
-      days: root.querySelector(".days"),
-      hours: root.querySelector(".hours"),
-      minutes: root.querySelector(".minutes"),
-      seconds: root.querySelector(".seconds"),
-      control: root.querySelector(".timer__btn--control"),
-      reset: root.querySelector(".timer__btn--reset"),
-    };
-    this.interval = null;
-    this.remainingSeconds = 90;
-    // this.remainingMinutes = 90;
-    // this.remainingHours = 90;
-    // this.remainingDays = 90;
-    // this.remainingWeeks = 90;
+//     this.el = {
+//       months: root.querySelector(".months"),
+//       weeks: root.querySelector(".weeks"),
+//       days: root.querySelector(".days"),
+//       hours: root.querySelector(".hours"),
+//       minutes: root.querySelector(".minutes"),
+//       seconds: root.querySelector(".seconds"),
+//       control: root.querySelector(".timer__btn--control"),
+//       reset: root.querySelector(".timer__btn--reset"),
+//     };
+//     this.interval = null;
+//     this.remainingSeconds = 0;
+  
 
-    this.updateInterfaceControls()
+//     // this.remainingMinutes = 90;
+//     // this.remainingHours = 90;
+//     // this.remainingDays = 90;
+//     // this.remainingWeeks = 90;
 
-    this.el.control.addEventListener("click", () => {
-     if (this.interval === null){
-        this.start();
-     }else{
-        this.stop();
-     }
-    });
-    this.el.reset.addEventListener("click", () => {
-        const inputMinutes = prompt("Enter number of minutes:")
-        if(inputMinutes < 60){
-     this.stop();
-     this.remainingSeconds = inputMinutes * 60;
-     this.updateInterfaceTime();
-    }
-    });
-  }
+//     // this.updateInterfaceTime();
 
+//     this.el.control.addEventListener("click", () => {
+//      if (this.interval === null){
+//         this.start();
+//      }else{
+//         this.stop();
+//      }
+//     });
+//       this.el.reset.addEventListener("click", () => {
+//           const inputMonths = prompt("Enter number of months:")
+//           if(inputMonths < 12){
+//        this.stop();
+//        this.remainingSeconds = inputMonths * 60;
+//        this.updateInterfaceTime();
+//       }
+//       });
+//   }
 
-    updateInterfaceTime(){
+//   // getRemainingTime() {
+//   //   let futureDate = new Date();
+//   //   let futureTime = futureDate.getTime();
+//   //   const today = new Date().getTime();
+//   //   const t = futureTime - today;
 
-        const months = Math.floor(this.remainingSeconds / 60)
-        const weeks = Math.floor(this.remainingSeconds / 60)
-        const days = Math.floor(this.remainingSeconds / 60)
-        const hours = Math.floor(this.remainingSeconds / 60)
-        const minutes = Math.floor(this.remainingSeconds / 60);
-        const seconds = this.remainingSeconds % 60;
-         
-        // console.log(months, weeks, days, hours, minutes, seconds);
+//   //   // console.log(t);
 
-        this.el.months.textContent = months.toString().padStart(2, '0');
-        this.el.weeks.textContent = weeks.toString().padStart(2, '0');
-        this.el.days.textContent = days.toString().padStart(2, '0');
-        this.el.hours.textContent = hours.toString().padStart(2, '0');
-        this.el.minutes.textContent = minutes.toString().padStart(2, '0');
-        this.el.seconds.textContent = seconds.toString().padStart(2, '0');
-    }
+//   //       // 1s = 1000ms
+//   //       // 1m = 60s
+//   //       // 1hr = 60min
+//   //       // 1d = 24hr
+//   //       // values in ms
+//   //       const oneMonth = 4 * 7 * 24 * 60 * 60 * 1000;
+//   //       const oneWeek = 7 * 24 * 60 * 60 * 1000;
+//   //       const oneDay = 24 * 60 * 60 * 1000;
+//   //       const oneHour = 60 * 60 * 1000;
+//   //       const oneMinute = 60 * 1000;
+//   //       // calculate all value
+//   //       let months = t / oneMonth;
+//   //       months = Math.floor(months);
+//   //       let weeks = Math.floor((t % oneMonth) / oneWeek)
+//   //       let days = Math.floor((t % oneWeek) / oneDay)
+//   //       let hours = Math.floor((t % oneDay) / oneHour);
+//   //       let minutes = Math.floor((t % oneHour) / oneMinute);
+//   //       let seconds = Math.floor((t % oneMinute) / 1000);
+//   //       console.log(months, weeks, days, hours, minutes, seconds);
+//   // }
 
-        updateInterfaceControls(){
-            if (this.interval === null){
-                // this.el.control.innerHTML = `<i class="bi bi-play-fill"></i>`
-                this.el.control.classList.add("bi-play-fill")
-                this.el.control.classList.remove("bi-pause-fill")
-            }else{
-                // this.el.control.innerHTML = `<i class="bi bi-pause-fill"></i>`
-                this.el.control.classList.remove("bi-play-fill")
-                this.el.control.classList.add("bi-pause-fill")
-            }
-        }
+//   updateInterfaceTime() {
+//     // this.getRemainingTime()
+//     let futureDate = new Date();
+//     let futureTime = futureDate.getTime();
+//     const today = new Date().getTime();
+//     const t = futureTime - today;
 
+//     const oneMonth = 4 * 7 * 24 * 60 * 60 * 1000;
+//     const oneWeek = 7 * 24 * 60 * 60 * 1000;
+//     const oneDay = 24 * 60 * 60 * 1000;
+//     const oneHour = 60 * 60 * 1000;
+//     const oneMinute = 60 * 1000;
+//         // calculate all value
+//         let months = t / oneMonth;
+//         months = Math.floor(months);
+//         let weeks = Math.floor((t % oneMonth) / oneWeek)
+//         let days = Math.floor((t % oneWeek) / oneDay)
+//         let hours = Math.floor((t % oneDay) / oneHour);
+//         let minutes = Math.floor((t % oneHour) / oneMinute);
+//         let seconds = Math.floor((t % oneMinute) / 1000);
 
-        start (){
-            if (this.remainingSeconds === 0) return;
+//     console.log(months, weeks, days, hours, ':' +  minutes, ':' + seconds);
 
-            this.interval = setInterval(() => {
-                this.remainingSeconds--;
-                this.updateInterfaceTime();
+//     this.el.months.textContent = months.toString().padStart(2, '0');
+//     this.el.weeks.textContent = weeks.toString().padStart(2, '0');
+//     this.el.days.textContent = days.toString().padStart(2, '0');
+//     this.el.hours.textContent = hours.toString().padStart(2, '0');
+//     this.el.minutes.textContent = minutes.toString().padStart(2, '0');
+//     this.el.seconds.textContent = seconds.toString().padStart(2, '0');
+//   }
 
-                if (this.remainingSeconds === 0) {
-                    this.stop();
-                }
-            }, 1000);
+//   updateInterfaceControls(){
+//       if (this.interval === null){
+//           // this.el.control.innerHTML = `<i class="bi bi-play-fill"></i>`
+//           this.el.control.classList.add("bi-play-fill")
+//           this.el.control.classList.remove("bi-pause-fill")
+//       }else{
+//           // this.el.control.innerHTML = `<i class="bi bi-pause-fill"></i>`
+//           this.el.control.classList.remove("bi-play-fill")
+//           this.el.control.classList.add("bi-pause-fill")
+//       }
+//   }
 
-            this.updateInterfaceControls();
-        }
+//   start (){
+//       if (this.remainingSeconds === 0) return;
 
-    stop() {
-        clearInterval(this.interval);
+//       this.interval = setInterval(() => {
+//           this.remainingSeconds--;
+//           this.updateInterfaceTime();
 
-        this.interval = null;
+//           if (this.remainingSeconds === 0) {
+//               this.stop();
+//           }
+//       }, 1000);
 
-        this.updateInterfaceControls();
-    }
+//       this.updateInterfaceControls();
+//   }
 
+//   stop() {
+//       clearInterval(this.interval);
 
-  static getHtml() {
-    return ` <div class="count_text">
-        <i class="uil uil-exclamation-triangle"></i>
-        <p>Countdown to Exam</p>
-      </div>
+//       this.interval = null;
 
-      <div class="count_input">
-        <div class="mwd">
-          <div class="time">
-            <span class="num months">00</span>
-            <span class="text">months</span>
-          </div>
-          <p class="semiColoumn">:</p>
-          <div class="time">
-            <span class="num weeks">00</span>
-            <span class="text">weeks</span>
-          </div>
-          <p class="semiColoumn">:</p>
-          <div class="time">
-            <span class="num days">00</span>
-            <span class="text">days</span>
-          </div>
-        </div>
+//       this.updateInterfaceControls();
+//   }
 
-        <div class="hms">
-          <span class="hours">00 :</span>
-          <span class="minutes">00 :</span>
-          <span class="seconds">00</span>
-        </div>
-        <i class="bi bi-alarm timer__btn--reset"></i>
-        <i class="bi bi-play-fill timer__btn--control"></i>
-      </div>`;
-  }
-}
+//   static getHtml() {
+//     return ` <div class="count_text">
+//         <i class="uil uil-exclamation-triangle"></i>
+//         <p>Countdown to Exam</p>
+//       </div>
+
+//       <div class="count_input">
+//         <div class="mwd">
+//           <div class="time">
+//             <span class="num months">00</span>
+//             <span class="text">months</span>
+//           </div>
+//           <p class="semiColoumn">:</p>
+//           <div class="time">
+//             <span class="num weeks">00</span>
+//             <span class="text">weeks</span>
+//           </div>
+//           <p class="semiColoumn">:</p>
+//           <div class="time">
+//             <span class="num days">00</span>
+//             <span class="text">days</span>
+//           </div>
+//         </div>
+
+//         <div class="hms">
+//           <span class="hours">00 :</span>
+//           <span class="minutes">00 :</span>
+//           <span class="seconds">00</span>
+//         </div>
+//         <i class="bi bi-alarm timer__btn--reset"></i>
+//         <i class="bi bi-play-fill timer__btn--control"></i>
+//       </div>`;
+//   }
+// }
--- Assets/js/dash.js
+++ Assets/js/dash.js
@@ -1,5 +1,100 @@
-import Timer from "./Timer.js";
 
-new Timer(
-    document.querySelector(".countdown")
-);
+
+const timeElement = document.querySelector('.time_start')
+const dateElement = document.querySelector('.date_start')
+
+
+/** 
+ * @param{Date} date
+ */
+function formatTime(date){
+  const hours12 = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const isAm = date.getHours() < 12;
+
+  return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
+
+/** 
+ * @param{Date} date
+ */
+
+function formatDate(date){
+  const Months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${DAYS[date.getDay()]}, ${Months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+}
+
+
+setInterval(() => {
+  const current = new Date();
+  
+
+  timeElement.textContent = formatTime(current);
+  dateElement.textContent = formatDate(current);
+  
+}, 200);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Timer from "./Timer.js";
+
+// new Timer(
+//     document.querySelector(".countdown")
+// );
